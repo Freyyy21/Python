5dcd54c5e55e754700dec18a87282b299e8a92d1
Freyyy21 <dgraymaiden@gmail.com>
2025-11-29 04:30:39 +0700

    Initial Commit

---
+++ Basic/Syntax.py
@@ -0,0 +1,12 @@
+#In Python, variables are createed when you assign a value to it:
+x = 5
+y = 2
+
+#Python Indentation
+#Python uses indentation to indicate a block of code.
+#Good Example
+if x > y:
+    print("x is greater than y")
+#Bad Example
+#if x < y:
+#print("x is less than y")
